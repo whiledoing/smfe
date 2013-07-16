1982afa97b1c10df2ddf59b88fd6f83931566c54
whiledoing <lovingwhile@gmail.com>
2013-07-16 17:00:22 +0800

    add integration_with_delta func

--- include/smfe/integral_calculus.h
+++ include/smfe/integral_calculus.h
@@ -24,7 +24,9 @@ namespace smfe
  * 可以参考:http://en.wikipedia.org/wiki/Newton%E2%80%93Cotes_formulas
  *
  * @param data 输入数据
- * @param degree 计算需要的阶数,有效范围[2,4]
+ * @param degree 计算需要的阶数
+ *
+ * @pre degree的范围为[2,4]
  */
 template<typename ContainterType>
 value_t integration(ContainterType const& data, int degree = 2)
@@ -35,7 +37,7 @@ value_t integration(ContainterType const& data, int degree = 2)
 
     value_t sum = 0.0;
 
-    for(int i = degree-1; i < data.size() - (degree-1); ++i)
+    for(size_t i = degree-1; i < data.size() - (degree-1); ++i)
         sum += data[i];
 
     if(degree == 2) {
@@ -44,13 +46,39 @@ value_t integration(ContainterType const& data, int degree = 2)
 
     if(degree == 3) {
         sum += (data[0] + 5*data[1] + 5*data[data.size()-2] + data[data.size()-1]) / 6;
-        return sum;
     }
 
     if(degree == 4) {
         sum += (data[0] + 4*data[1] + 7*data[2] + 7*data[data.size()-3] + 4*data[data.size()-2] + data[data.size()-1]) / 8;
-        return sum;
     }
+
+    return sum;
+}
+
+/**
+ *	计算非分等间隔划分数据的积分
+ *	
+ *	1.	两个相邻离散数据的间隔有参数delta_vec给出
+ *	2.	数据data[1]和data[0]之间的间隔为delta_vec[0],	data[2]和data[1]之间间隔为delta_vec[1],etc...
+ *	3.	使用degree为2的积分算法
+ *	
+ *	@param data 被积分数据
+ *	@param delta_vec 积分数据之间间隔
+ *	
+ *	@pre <strong class="paramname">data</strong>的数据长度比<strong class="paramname">delta_vec</strong>的长度大一
+ */
+template<typename ContainterType>
+value_t integration_with_delta(ContainterType const& data, ContainterType const& delta_vec)
+{
+    CHECK_VALUE_TYPE(data);
+    CHECK_VALUE_TYPE(delta_vec);
+
+	BOOST_ASSERT(data.size() - 1 == delta_vec.size());
+
+    value_t sum = 0.0;
+
+    for(size_t i = 0u; i < data.size() - 1; ++i)
+        sum += (data[i] + data[i+1]) / 2 * delta_vec[i];
 
     return sum;
 }
